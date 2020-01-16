3fb10ab08d7d7e425c29a13f1beb2115ffd24b1f
Austin Thresher <athresher@lanl.gov>
2020-01-16 10:32:25 -0700

    Added currently playing track in Ampache to conky. Added fonts

---
+++ scripts/get-playing-ampache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Awk script to grab song title from Ampache window title.
+# This script only works if Ampache is the focused tab or has its own window.
+# The regex matches from the beginning of the string to the first few characters
+# of ' | Ampache :: For the Love of Music'. Then it trims the beginning of the
+# string to the first quotation mark, leaving us with just the title and artist
+read -d '' awk_script << 'EOF'
+{
+	match(\$0, "^ *[0-9]x[0-9a-fA-F]* \\".* \\| ")
+	trim_from_start = index($0, "\\"")
+	print(substr($0, RSTART+trim_from_start, RLENGTH-3-trim_from_start))
+}
+EOF
+
+xwininfo -tree -root | grep " | Ampache" | tail -n 1 | awk "$awk_script"
--- scripts/i3-workspace.sh
+++ scripts/i3-workspace.sh
@@ -2,18 +2,18 @@
 __WINDEX=$(i3-msg -t get_workspaces \
 	| sed 's|.*name\":\"\([a-zA-Z0-9]*\)\",\"visible\":true.*|\1|')
 
-printf '${color #666666}'
+printf '${color1}'
 for ((i=1;i<=9;i++)); do
 	if [ "$i" == "$__WINDEX" ]; then
-		printf ' ${color white}'
+		printf ' ${color0}'
 		printf "$i"
-		printf '${color #666666}'
+		printf '${color1}'
 	else
 		printf " $i"
 	fi
 done
 if [ "$__WINDEX" == 10 ]; then
-	printf ' ${color white}0'
+	printf ' ${color0}0'
 else
 	printf ' 0'
 fi
