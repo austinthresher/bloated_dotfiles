f6cda61f10f86df23d7bf34fc791e1906c9b7625
Austin Thresher <austin@dangermomentum.com>
2020-01-19 23:16:25 -0700

    Fixed bug in currently playing song display

--- scripts/get-playing-ampache.sh
+++ scripts/get-playing-ampache.sh
@@ -7,11 +7,10 @@
 # string to the first quotation mark, leaving us with just the title and artist
 read -d '' awk_script << 'EOF'
 {
-#	match(\$0, "^ *[0-9]x[0-9a-fA-F]* \\".* \\\| ")
-#	trim_from_start = index($0, "\\"")
-#	print(substr($0, RSTART+trim_from_start, RLENGTH-3-trim_from_start))
-	print("ampache")
+	match($0, "^ *[0-9]x[0-9a-fA-F]* \\".*Ampache")
+	trim_from_start = index($0, "\\"")
+	print(substr($0, RSTART+trim_from_start, RLENGTH-10-trim_from_start))
 }
 EOF
 
-xwininfo -tree -root | grep " | Ampache" | tail -n 1 | awk "$awk_script"
+xwininfo -tree -root | grep " | Ampache" | tail -n 1 | gawk "$awk_script"
