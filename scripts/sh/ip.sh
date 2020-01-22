d8153d1962bade6ae1f9d2931fc6821dc3282e04
Austin Thresher <athresher@lanl.gov>
2020-01-22 10:14:24 -0700

    Added basic ifconfig support to ip.sh

--- scripts/sh/ip.sh
+++ scripts/sh/ip.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 function get_ip() {
-	if [ -x "$(which ip)" ]; then
+	if [ ! -z "$(which ip)" ]; then
 		ip addr \
 			| egrep "(wlp|wlan|enp|eth|en|eno)[0-9]" \
 			| egrep "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" \
 			| awk '{ print $2 }' \
 			| sed 's/\/.*//g'
+	elif [ ! -z "$(which ifconfig)" ]; then
+		ifconfig \
+			| grep 'inet [0-9]*.[0-9]*.[0-9]*.[0-9]' \
+			| grep -v '127.0.0.1' \
+			| awk '{ print $2 }'
 	else
 		printf "no ip"
 	fi
