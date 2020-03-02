f5be5a796c15300e3fb16944a52a57c79228ceb7
Austin Thresher <athresher@lanl.gov>
2020-03-02 08:52:03 -0700

    Fix bug when ide-coc is ran without a functioning coc install

--- scripts/installers/ide-coc.sh
+++ scripts/installers/ide-coc.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd "$HOME/.config/nvim/repos/"
+if [ ! -d "coc.nvim" ]; then
+    git clone https://github.com/neoclide/coc.nvim coc.nvim
+fi
+cd coc.nvim
+git checkout release
+
 EXTENSIONS='coc-json coc-tsserver coc-yaml coc-python coc-highlight '
 EXTENSIONS+='coc-snippets coc-lists coc-yank coc-vimlsp coc-clangd '
 
