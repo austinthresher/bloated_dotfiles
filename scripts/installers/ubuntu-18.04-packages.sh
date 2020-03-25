24a2268264c295a3c469371b32b353b5b3f211e8
Austin Thresher <austin@dangermomentum.com>
2020-03-25 10:34:33 -0600

    changed vim keybinds

--- scripts/installers/ubuntu-18.04-packages.sh
+++ scripts/installers/ubuntu-18.04-packages.sh
@@ -2,6 +2,6 @@
 apt-get update
 apt-get install -y \
     vim tmux git make build-essential \
-    python3 graphviz doxygen cppcheck \
+    python3 python3-pip graphviz doxygen cppcheck \
     libsdl2-dev libsdl2-ttf-dev tree indent \
-    ncurses-term stterm silversearcher-ag
+    ncurses-term silversearcher-ag curl
